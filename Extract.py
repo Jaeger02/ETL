0f6eda1e76c1229c7de5947cc37f8bf96d92897e
Jaeger02 <aduaperd@gmail.com>
2022-04-06 17:10:11 -0500

    v3

--- Extract.py
+++ Extract.py
@@ -1,2 +1,10 @@
+import boto3
 class Extract:
-    
+    __arg_date = ""
+    __src_format = ''
+    __src_bucket = ''
+    __trg_bucket = ''
+    __columns = []
+    __key = ''
+    __s3 = boto3.resource('s3')
+
